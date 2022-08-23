4d498a351f26cd197cd905995dc57cab131bd253
Mariana Muñoz <mariana412@gmail.com>
2022-08-22 20:00:51 -0500

    New

---
+++ parte1/js/script.js
@@ -0,0 +1,7 @@
+var n=0;
+function incrementa()
+{
+    n++;
+    var elemento=document.querySelector("#numero");
+    elemento.innerText=n;
+}
---
+++ parte2/js/script.js
@@ -0,0 +1,23 @@
+var n1=0;
+function incrementa1()
+{
+    n1++;
+    var elemento=document.querySelector("#numero1");
+    elemento.innerText=n1;
+}
+
+var n2=0;
+function incrementa2()
+{
+    n2++;
+    var elemento=document.querySelector("#numero2");
+    elemento.innerText=n2;
+}
+
+var n3=0;
+function incrementa3()
+{
+    n3++;
+    var elemento=document.querySelector("#numero3");
+    elemento.innerText=n3;
+}
